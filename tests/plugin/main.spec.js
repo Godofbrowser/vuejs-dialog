d5ffb5f59558d2170c0dcb4a0e4ec2ed4aa76f26
Ajeh Emeke <ajemeke@gmail.com>
2017-10-08 20:34:24 +0100

    Thinned test.

--- tests/plugin/main.spec.js
+++ tests/plugin/main.spec.js
@@ -6,57 +6,56 @@ import Plugin from '../../src/plugin/js/index'
 import should from 'should'
 import {assert} from 'chai'
 
-describe('Plugin', function () {
-    describe('#installer', function () {
-        it('"Plugin.install" Should be a function', function () {
-            should(Plugin.install).be.a.Function()
-        })
+describe('Plugin installer', function () {
+    it('"Plugin.install" Should be a function', function () {
+        should(Plugin.install).be.a.Function()
     })
+})
 
-    describe('#Installed', function () {
-        it('Plugin Should be installed', function () {
-            const Vue = require('vue').default
-            Vue.use(Plugin)
+describe('Plugin Installed', function () {
+    it('Plugin Should be installed', function () {
+        const Vue = require('vue').default
+        Vue.use(Plugin)
 
-            assert.property(Vue, 'dialog')
-        })
+        assert.property(Vue, 'dialog')
     })
+})
 
-    describe('#Available', function () {
-        it('"$dialog" Should be a available on the created, mounted hooks', function () {
-            const Vue = require('vue').default
-            Vue.use(Plugin)
+describe('Plugin Available', function () {
+    it('"$dialog" Should be a available on the created, mounted hooks', function () {
+        const Vue = require('vue').default
+        Vue.use(Plugin)
 
-            new Vue({
-                created(){
+        new Vue({
+            created(){
+                assert.property(this, '$dialog')
+            },
+            mounted(){
+                assert.property(this, '$dialog')
+            },
+            render(h){
+                return h('p', {id: 'test'}, 'test')
+            }
+        }).$mount()
+    })
+})
+
+describe('Plugin Available', function () {
+    it('"$dialog" Should be a available in component methods', function () {
+        const Vue = require('vue').default
+        Vue.use(Plugin)
+
+        let vm = new Vue({
+            methods: {
+                check(){
                     assert.property(this, '$dialog')
-                },
-                mounted(){
-                    assert.property(this, '$dialog')
-                },
-                render(h){
-                    return h('p', {id: 'test'}, 'test')
                 }
-            }).$mount()
-        })
+            },
+            render(h){
+                return h('p', {id: 'test'}, 'test')
+            }
+        }).$mount()
 
-        it('"$dialog" Should be a available in component methods', function () {
-            const Vue = require('vue').default
-            Vue.use(Plugin)
-
-            let vm = new Vue({
-                methods: {
-                    check(){
-                        assert.property(this, '$dialog')
-                    }
-                },
-                render(h){
-                    return h('p', {id: 'test'}, 'test')
-                }
-            }).$mount()
-
-            vm.check()
-        })
+        vm.check()
     })
-
 })
