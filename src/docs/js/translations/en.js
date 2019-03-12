53fd0f0fbe047223f0c2be7391b2c501e549b4c9
Ajeh Emeke <Ajemeke@gmail.com>
2019-03-12 12:18:26 +0100

    Added prompt (#48)
    * Added prompt
    
    * Update readme with prompt example
    
    * Build scripts
    
    * 1.4.0
    
    * Update translation

--- src/docs/js/translations/en.js
+++ src/docs/js/translations/en.js
@@ -1,72 +1,74 @@
 // translations for English
 
 export default {
-    content: {
-        words: {
-            animation: "animation",
-            fade: "fade",
-            bounce: "bounce",
-            submit: "submit",
-            reset: "Reset",
-            continue: "continue",
-            proceed: "continue",
-            dismiss: "dismiss",
-            source_code: "Source Code"
-        },
-        titles: {
-            method_usage: "Usage as a method",
-            directive_usage: "Usage as a directive",
-            confirmation_types: "Confirmation types",
-        },
-        descriptions: {
-            confirmation_types: {
-                1: "Sometimes, you may want to be more strict by making sure a user really wants to proceed.",
-                2: "The SOFT and HARD confirmation dialogs helps with that."
-            },
-        },
-        examples: {
-            method_usage: {
-                1: "Alert Dialog - one button",
-                2: "Html Dialog - style/format content",
-                3: "Basic confirm - close instantly",
-                4: "Loading Dialog - Useful with ajax",
-                5: "Reversed Dialog - switch buttons",
-                6: "Fade Dialog - Animation",
-                7: "Bounce Dialog - Animation",
-                8: 'Close when clicking outside dialog'
-            },
-            directive_usage: {
-                1: "Give it a string",
-                2: "Go to example.com",
-                3: "Give it an object v-confirm=\"messageAndCallback\""
-            },
-            confirmation_types: {
-                1: "Soft confirm - multiple clicks required",
-                2: "Hard confirm - verification text required"
-            }
-        }
+  content: {
+    words: {
+      animation: 'animation',
+      fade: 'fade',
+      bounce: 'bounce',
+      submit: 'submit',
+      reset: 'Reset',
+      continue: 'continue',
+      proceed: 'continue',
+      dismiss: 'dismiss',
+      source_code: 'Source Code'
     },
-
-    messages: {
-        alert: 'This is an alert dialog. Click the button below to close.',
-        html: `This dialog has <b class="dg-highlight-1">HTML</b> enabled. Click the button below to close.`,
-        basic: 'This is a basic confirmation dialog. Clicking on either button dismisses the dialog instantly',
-        soft: 'This is a soft confirmation dialog. Multiple clicks required to proceed.',
-        hard: `This is a hard confirmation dialog. User\'s input must match <span class="dg-highlight-1">verification</span> <span class="dg-highlight-1">text</span> in order to proceed.`,
-        loading: `This is a loading dialog. The loader will start once you Click on Proceed button`,
-        reverse: `Left button is the Proceed button in this dialog. Click to have a feel of the reverse buttons`,
-        click_continue: `You clicked continue`,
-        click_cancel: `Closed because cancel was clicked`,
-        loading_completed: `If you see me, then the loader has completed`,
-        loading_canceled: `You clicked cancel, so there was no loading`,
-        directive_object: "This dialog was also triggered using a v-confirm directive. It has both OK and CANCEL callback",
-        directive_string: 'This is a message.',
-        directive_link: 'This will take you to "http://example.com". Proceed with caution',
-        form_reset: 'Changes would be discarded. Reset this form?',
-        form_submit: 'Submit this form?',
-        empty_name: "The name field is empty"
+    titles: {
+      method_usage: 'Usage as a method',
+      directive_usage: 'Usage as a directive',
+      confirmation_types: 'Confirmation types',
     },
-    placeholders: {
-        your_name: "Your name"
+    descriptions: {
+      confirmation_types: {
+        1: 'Sometimes, you may want to be more strict by making sure a user really wants to proceed.',
+        2: 'The SOFT and HARD confirmation dialogs helps with that.'
+      },
+    },
+    examples: {
+      method_usage: {
+        1: 'Alert Dialog - one button',
+        2: 'Html Dialog - style/format content',
+        3: 'Basic confirm - close instantly',
+        4: 'Loading Dialog - Useful with ajax',
+        5: 'Reversed Dialog - switch buttons',
+        6: 'Fade Dialog - Animation',
+        7: 'Bounce Dialog - Animation',
+        8: 'Close when clicking outside dialog',
+        9: 'Prompt - collect input'
+      },
+      directive_usage: {
+        1: 'Give it a string',
+        2: 'Go to example.com',
+        3: 'Give it an object v-confirm="messageAndCallback"'
+      },
+      confirmation_types: {
+        1: 'Soft confirm - multiple clicks required',
+        2: 'Hard confirm - verification text required'
+      }
     }
+  },
+
+  messages: {
+    alert: 'This is an alert dialog. Click the button below to close.',
+    prompt: 'What is the most important thing in life?',
+    html: `This dialog has <b class="dg-highlight-1">HTML</b> enabled. Click the button below to close.`,
+    basic: 'This is a basic confirmation dialog. Clicking on either button dismisses the dialog instantly',
+    soft: 'This is a soft confirmation dialog. Multiple clicks required to proceed.',
+    hard: `This is a hard confirmation dialog. User\'s input must match <span class="dg-highlight-1">verification</span> <span class="dg-highlight-1">text</span> in order to proceed.`,
+    loading: `This is a loading dialog. The loader will start once you Click on Proceed button`,
+    reverse: `Left button is the Proceed button in this dialog. Click to have a feel of the reverse buttons`,
+    click_continue: `You clicked continue`,
+    click_cancel: `Closed because cancel was clicked`,
+    loading_completed: `If you see me, then the loader has completed`,
+    loading_canceled: `You clicked cancel, so there was no loading`,
+    directive_object: 'This dialog was also triggered using a v-confirm directive. It has both OK and CANCEL callback',
+    directive_string: 'This is a message.',
+    directive_link: 'This will take you to "http://example.com". Proceed with caution',
+    form_reset: 'Changes would be discarded. Reset this form?',
+    form_submit: 'Submit this form?',
+    empty_name: 'The name field is empty'
+  },
+  placeholders: {
+    your_name: 'Your name'
+  }
 }
--- src/docs/js/translations/zh.js
+++ src/docs/js/translations/zh.js
@@ -1,71 +1,74 @@
 // translations for Chinese
 
 export default {
-    content: {
-        words: {
-            animation: "动画",
-            fade: "褪色",
-            bounce: "弹跳",
-            submit: "提交",
-            reset: "重启",
-            continue: "继续",
-            proceed: "继续",
-            dismiss: "解雇",
-            source_code: "源代码"
-        },
-        titles: {
-            method_usage: "用法作为一种方法",
-            directive_usage: "用法作为指令",
-            confirmation_types: "确认类型",
-        },
-        descriptions: {
-            confirmation_types: {
-                1: "有时，您可能希望通过确保用户真正想要继续进行更严格",
-                2: "柔软和硬确认对话框有助于此。"
-            },
-        },
-        examples: {
-            method_usage: {
-                1: "警报对话框 - 一个按钮",
-                2: "HTML对话框 - 风格/格式内容",
-                3: "基本确认 - 立即关闭",
-                4: "加载对话框 - 与ajax有用",
-                5: "反转对话框 - 开关按钮",
-                6: "淡化对话框 - 动画",
-                7: "反弹对话框 - 动画"
-            },
-            directive_usage: {
-                1: "给它一个字符串",
-                2: "转到example.com",
-                3: "给它一个对象"
-            },
-            confirmation_types: {
-                1: "软确认 - 需要多次点击",
-                2: "硬确认 - 需要验证文本"
-            }
-        }
+  content: {
+    words: {
+      animation: '动画',
+      fade: '褪色',
+      bounce: '弹跳',
+      submit: '提交',
+      reset: '重启',
+      continue: '继续',
+      proceed: '继续',
+      dismiss: '解雇',
+      source_code: '源代码'
     },
-
-    messages: {
-        alert: '这是一个警报对话框。 点击下面的按钮关闭。',
-        html: `此对话框启用了<b class="dg-highlight-1">超文本标记语言</b>。 点击下面的按钮关闭。`,
-        basic: '这是一个基本的确认对话框。 点击任一按钮立即关闭对话框',
-        soft: '这是一个软确认对话框。 需要多次点击才能继续。',
-        hard: `这是一个难以确认的对话框。用户的输入必须匹配 <span class="dg-highlight-1">验证</span> <span class="dg-highlight-1">文本</span>为了继续。`,
-        loading: `这是一个加载对话框。 点击继续按钮后，加载程序将启动`,
-        reverse: `左侧按钮是此对话框中的“继续”按钮。 点击以获得反向按钮的感觉`,
-        click_continue: `你点击继续`,
-        click_cancel: `关闭，因为已被点击`,
-        loading_completed: `如果你看到我，那么装载机已经完成了`,
-        loading_canceled: `你点击取消，所以没有加载`,
-        directive_object: "此对话框也使用v-confirm指令触发。 它有OK和CANCEL回调",
-        directive_string: '这是一个消息',
-        directive_link: '这将带你到 "http://example.com". 请谨慎行事',
-        form_reset: '更改将被丢弃。 重设此表格？',
-        form_submit: '提交此表格？',
-        empty_name: "名称字段为空"
+    titles: {
+      method_usage: '用法作为一种方法',
+      directive_usage: '用法作为指令',
+      confirmation_types: '确认类型',
     },
-    placeholders: {
-        your_name: "你的名字"
+    descriptions: {
+      confirmation_types: {
+        1: '有时，您可能希望通过确保用户真正想要继续进行更严格',
+        2: '柔软和硬确认对话框有助于此。'
+      },
+    },
+    examples: {
+      method_usage: {
+        1: '警报对话框 - 一个按钮',
+        2: 'HTML对话框 - 风格/格式内容',
+        3: '基本确认 - 立即关闭',
+        4: '加载对话框 - 与ajax有用',
+        5: '反转对话框 - 开关按钮',
+        6: '淡化对话框 - 动画',
+        7: '反弹对话框 - 动画',
+        8: '单击外部对话框时关闭',
+        9: '提示 - 收集输入'
+      },
+      directive_usage: {
+        1: '给它一个字符串',
+        2: '转到example.com',
+        3: '给它一个对象'
+      },
+      confirmation_types: {
+        1: '软确认 - 需要多次点击',
+        2: '硬确认 - 需要验证文本'
+      }
     }
+  },
+
+  messages: {
+    alert: '这是一个警报对话框。 点击下面的按钮关闭。',
+    prompt: '生命中最重要的是什么？',
+    html: `此对话框启用了<b class="dg-highlight-1">超文本标记语言</b>。 点击下面的按钮关闭。`,
+    basic: '这是一个基本的确认对话框。 点击任一按钮立即关闭对话框',
+    soft: '这是一个软确认对话框。 需要多次点击才能继续。',
+    hard: `这是一个难以确认的对话框。用户的输入必须匹配 <span class="dg-highlight-1">验证</span> <span class="dg-highlight-1">文本</span>为了继续。`,
+    loading: `这是一个加载对话框。 点击继续按钮后，加载程序将启动`,
+    reverse: `左侧按钮是此对话框中的“继续”按钮。 点击以获得反向按钮的感觉`,
+    click_continue: `你点击继续`,
+    click_cancel: `关闭，因为已被点击`,
+    loading_completed: `如果你看到我，那么装载机已经完成了`,
+    loading_canceled: `你点击取消，所以没有加载`,
+    directive_object: '此对话框也使用v-confirm指令触发。 它有OK和CANCEL回调',
+    directive_string: '这是一个消息',
+    directive_link: '这将带你到 "http://example.com". 请谨慎行事',
+    form_reset: '更改将被丢弃。 重设此表格？',
+    form_submit: '提交此表格？',
+    empty_name: '名称字段为空'
+  },
+  placeholders: {
+    your_name: '你的名字'
+  }
 }
