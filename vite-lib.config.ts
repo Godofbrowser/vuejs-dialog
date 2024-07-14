33b6e66cf682b9b8aad5d681183adce980a851d8
Emeke Ajeh <ajemeke@gmail.com>
2024-07-14 12:35:42 +0100

    Chore: add lib build configuration

---
+++ vite-lib.config.ts
@@ -0,0 +1,39 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import viteConfig from "./vite.config";
+import {mergeConfig} from "vitest/config";
+import pkg from './package.json'
+
+const packageName = pkg.name;
+const packageDeclarationName = 'VuejsDialog';
+
+export default mergeConfig(
+    viteConfig,
+    defineConfig({
+    build: {
+        copyPublicDir: false,
+        outDir: 'dist-lib',
+        lib: {
+            // Could also be a dictionary or array of multiple entry points
+            entry: resolve(__dirname, 'src/plugin/index.ts'),
+            name: packageDeclarationName,
+            fileName: (format) => packageName + `.${format}.js`,
+            formats: ['es', 'umd'],
+        },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: ['vue'],
+            output: {
+                globals: {
+                    vue: 'Vue',
+                },
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name == "style.css") return packageName +".min.css";
+                    return assetInfo.name;
+                },
+            },
+        },
+    },
+}))
