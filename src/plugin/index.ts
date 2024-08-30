6c7a07e0d601b74ec05ef54ff2464dc497141cd6
Emeke Ajeh <ajemeke@gmail.com>
2024-08-30 20:55:22 +0100

    Fix(directive): resolve breaking changes in directive related code

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -12,7 +12,7 @@ interface DialogPluginOptions extends Omit<DialogWindowOptionsInterface, 'id'>{}
 const DialogPlugin = {
     install(app: App, options: DialogPluginOptions) {
         const DirectivesObj = new DirectiveDialog(app)
-        app.directive('confirm', DirectivesObj.confirmDefinition)
+        app.directive('confirm', DirectivesObj.defineConfirm())
 
         const dialog = new PromiseDialog(app, options)
 
