b6f7c471c5fd8d7ba508295048be18d72583be50
Emeke Ajeh <ajemeke@gmail.com>
2024-09-06 20:30:13 +0100

    Chore(app): demo app clean-up

---
+++ src/views/CompView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="index">
+    <h1 style="text-align: center;margin-bottom: 35px;margin-top: 35px;">Comp page</h1>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { inject } from "vue";
+
+const dialog = inject('$dialog')
+const openDialog = () => dialog.alert('Hello world!')
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .index {
+    max-width: 700px;
+  }
+}
+</style>
