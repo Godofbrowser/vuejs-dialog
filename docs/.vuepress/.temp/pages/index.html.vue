59ab5ef8e0f2af27ed026c6a660e21bf5ee457a9
Emeke Ajeh <ajemeke@gmail.com>
2024-09-06 20:48:41 +0100

    Chore(docs): remove temp directories

--- docs/.vuepress/.temp/pages/index.html.vue
+++
@@ -1,7 +0,0 @@
-<template><div><h1 id="hello-vuepress" tabindex="-1"><a class="header-anchor" href="#hello-vuepress"><span>Hello VuePress</span></a></h1>
-<p>One plus one equals: {{ 1 + 1 }}</p>
-<p><span v-for="i in 3"> span: {{ i }} </span></p>
-<p><button class="dg-btn" v-confirm="'Please confirm!'">Click Directive</button></p>
-</div></template>
-
-
