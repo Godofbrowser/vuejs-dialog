b0671d4422153f383d69b2b1513d05cb0ef917bf
Emeke Ajeh <ajemeke@gmail.com>
2024-09-06 20:12:43 +0100

    Chore(docs): add shiki plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,15 @@
 import { viteBundler } from '@vuepress/bundler-vite'
 import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
+import {shikiPlugin} from "@vuepress/plugin-shiki";
+// import { mdEnhancePlugin } from "vuepress-plugin-md-enhance";
+
+// Ref (default theme): https://ecosystem.vuejs.press/themes/default/markdown.html
 
 export default defineUserConfig({
+    title: 'Vuejs Dialog Plugin',
+    description: 'A lightweight, promise based alert, prompt and confirm dialog',
+    repo: 'godofbrowser/vuejs-dialog',
     bundler: viteBundler(),
     theme: defaultTheme(),
 })
