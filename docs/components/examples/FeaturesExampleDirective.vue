1dc23397b51362ac026900c7310117f7b91c63ce
Emeke Ajeh <ajemeke@gmail.com>
2024-09-08 01:16:14 +0100

    Chore(docs): add examples wrapper and document directive

---
+++ docs/components/examples/FeaturesExampleDirective.vue
@@ -0,0 +1,10 @@
+<template>
+  <a href="https://example.com"
+     target="_blank"
+     v-confirm="'Confirming this dialog will take you to an external website'"
+  >A link</a>
+  <span> or a </span>
+  <button v-confirm="'Confirm to change button text to \'Already clicked\''"
+          @click="($event) => $event.target.textContent = 'Already clicked'"
+  >button</button>
+</template>
