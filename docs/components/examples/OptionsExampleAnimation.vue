90137b7f8f13ad85e367d6392d130f16ce2384e2
Emeke Ajeh <ajemeke@gmail.com>
2024-09-30 00:26:19 +0100

    Chore(docs): update options section

---
+++ docs/components/examples/OptionsExampleAnimation.vue
@@ -0,0 +1,24 @@
+<template>
+  <UIExamplesButton @click="openDialog('zoom')">Zoom</UIExamplesButton>&nbsp;
+  <UIExamplesButton @click="openDialog('fade')">Fade</UIExamplesButton>&nbsp;
+  <UIExamplesButton @click="openDialog('bounce')">Bounce</UIExamplesButton>
+</template>
+
+<script setup>
+  import {inject} from 'vue'
+  import {injectionKey} from "../../../src/plugin/index.ts";
+
+  defineOptions({
+    name: "OptionsExampleAnimation"
+  })
+
+  const $dialog = inject(injectionKey)
+  const openDialog = (animation) => $dialog.alert({
+    title: 'Alert example',
+    body: 'Session expired. Please login again to continue.',
+  }, {
+    okText: 'Dismiss',
+    animation,
+    backdropClose: true,
+  })
+</script>
--- docs/features.md
+++ docs/features.md
@@ -1,4 +1,4 @@
-# Features
+# Basic Features
 
 [[toc]]
 
