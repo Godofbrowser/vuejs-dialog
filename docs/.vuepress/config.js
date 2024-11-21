9473e7e0b9c5b5c4fc3e112188c2f0dc428e442a
Emeke Ajeh <ajemeke@gmail.com>
2024-11-21 12:33:16 +0100

    chore: add GA plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,30 @@
 import { viteBundler } from '@vuepress/bundler-vite'
 import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
-import {shikiPlugin} from "@vuepress/plugin-shiki";
+import { shikiPlugin } from "@vuepress/plugin-shiki";
+import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
 // import { mdEnhancePlugin } from "vuepress-plugin-md-enhance";
 
 // Ref (default theme): https://ecosystem.vuejs.press/themes/default/markdown.html
 
+const isProd = process.env.NODE_ENV === 'production'
+
+const prodOnlyPlugins = []
+if (isProd) {
+    prodOnlyPlugins.push(
+        googleAnalyticsPlugin({
+            id: 'G-77TKFH0GYP',
+        })
+    )
+}
+
 export default defineUserConfig({
     title: 'Vuejs Dialog (Vue3)',
     description: 'A lightweight, promise based alert, prompt and confirm dialog',
-    base: process.env.NODE_ENV === 'production' ? '/vuejs-dialog/' : '/',
+    base: isProd ? '/vuejs-dialog/' : '/',
     bundler: viteBundler(),
     plugins: [
+        ...prodOnlyPlugins,
         // https://ecosystem.vuejs.press/plugins/markdown/prismjs.html
         // https://ecosystem.vuejs.press/plugins/markdown/shiki.html
         shikiPlugin({
